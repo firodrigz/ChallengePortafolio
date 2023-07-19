f57fca7f32c542c5d7b76d633f1ac5515b603497
firodrigz <firodrigz@gmail.com>
2023-07-19 16:00:32 -0300

    Menu js

--- menu.js
+++ menu.js
@@ -4,13 +4,13 @@ const button = document.querySelector('#menu__button');
 const nav = document.querySelector('.menu__header__nav');
 
 button.addEventListener('click',()=>{
-    button.classList.toggle('bx-x');
+    button.classList.toggle('fa-xmark');
     nav.classList.toggle('activo');
 })
 
 /*Remueve en scroll */
 
 window.onscroll = ()=>{
-    button.classList.remove('bx-x');
+    button.classList.remove('fa-xmark');
     nav.classList.remove('activo');
 }
